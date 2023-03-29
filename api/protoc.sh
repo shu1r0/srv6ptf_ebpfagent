a843c9f8f3dcd65847fd17bd3c026a01e77150a2
shu1r0 <shu1r0.net@gmail.com>
2023-03-29 15:25:07 +0900

    update proto

--- api/protoc.sh
+++ api/protoc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-protoc packet_collector.proto  --go_out=../pkg/api/ --go_opt=paths=source_relative --go-grpc_out=../pkg/api/ --go-grpc_opt=paths=source_relative
+protoc packet_collector.proto --proto_path=../nfagent/api --go_out=../pkg/api/ --go_opt=paths=source_relative --go-grpc_out=../pkg/api/ --go-grpc_opt=paths=source_relative
